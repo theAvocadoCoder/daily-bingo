58208d4f69895fb099b4cd0b17873ca8a0a98d82
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-08-27 01:47:47 +0100

    Implement mark bingo cell

--- pages/bingo.vue
+++ pages/bingo.vue
@@ -1,49 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import bingoCard from "./bingo.json"
 
-const card = ref([
-  [
-    "green car",
-    "man on a bike",
-    "fur hat",
-    "leaf with 3 holes",
-    "your mother's face",
-  ],
-  [
-    "",
-    "",
-    "",
-    "",
-    "",
-  ],
-  [
-    "",
-    "",
-    "",
-    "",
-    "",
-  ],
-  [
-    "",
-    "",
-    "",
-    "",
-    "",
-  ],
-  [
-    "",
-    "",
-    "",
-    "",
-    "",
-  ],
-]);
+const card = ref(bingoCard);
 
-function markCell(cellId) {
-}
+const rows = ref([null,[],[],[],[],[]]),
+columns = ref([null,[],[],[],[],[]]),
+fDiagonal = ref([]),
+bDiagonal = ref([]);
 
-function unMarkCell() {
-  squares.value.splice(selectedIndex.value, 1);
+function markCell(row,cellId) {
+  const cellIndex = card.value[row].indexOf(card.value[row].filter(c=>c.id===cellId)[0]);
+  const cell = card.value[row][cellIndex];
+  if (cellId !== "r3-cell-3") cell.class.includes("selected") 
+    ? cell.class = cell.class.replace("selected", "") 
+    : cell.class += " selected";
 }
   
 </script>
@@ -52,9 +23,9 @@ function unMarkCell() {
   <div id="card">
     <template v-for="row,rowid in card">
       <div :id="'row-'+(rowid+1)" class="row">
-        <template v-for="cell,cellid in row">
-          <div :id="'r'+(rowid+1)+'-cell-'+(cellid+1)" class="cell" @click="markCell(cellid)">
-            {{ cell }}
+        <template v-for="cell in row">
+          <div :id="cell.id" :class="cell.class" @click="markCell(cell.row - 1,cell.id)">
+            {{ cell.value }}
           </div>
         </template>
       </div>
@@ -63,15 +34,59 @@ function unMarkCell() {
 </template>
 
 <style>
+  #card {
+    border: 1px solid black;
+    width: min(99%,35rem);
+    height: min(90%,35rem);
+    padding: 0;
+    gap: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+    margin-inline: auto;
+  }
+
   .row {
     display: flex;
+    flex: 1;
+
+    &:not(:last-child) {
+      border-bottom: 1px solid black;
+    }
   }
   .cell {
     display: flex;
     align-items: center;
     justify-content: center;
+    text-align: center;
+    padding: .5rem;
     flex: 1;
     height: 100px;
     width: 100px;
+    font-size: .75rem;
+    cursor: pointer;
+    user-select: none;
+
+    &:not(:last-child) {
+      border-right: 1px solid black;
+    }
+
+    &:hover {
+      background-color: #00ccfe3d;
+    }
+  }
+
+  .selected {
+    position: relative;
+
+    &::after {
+      content: "";
+      position: absolute;
+      z-index: -1;
+      inset: 1rem;
+      background-color: #6df843;
+      border-radius: 50%;
+    }
   }
 </style>
