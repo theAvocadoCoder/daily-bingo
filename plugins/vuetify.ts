a93cf8a7434c690c1d8de102ff944df71ba4f3a3
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-08-31 04:41:37 +0100

    Set up Vuetify

---
+++ plugins/vuetify.ts
@@ -0,0 +1,13 @@
+// import this after install `@mdi/font` package
+import "@mdi/font/css/materialdesignicons.css";
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    // TODO: vuetify configuration
+  });
+  app.vueApp.use(vuetify);
+})
