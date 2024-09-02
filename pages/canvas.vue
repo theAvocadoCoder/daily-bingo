ebde9a4e909c71c1998ead8e5b364c50b345e5f4
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-02 01:32:28 +0100

    Refactor card to use canvas
    - Fix bug that causes canvas not to display until first click
    - Properly handle resizing
    -  Allow text to wrap when too long for one line
    - Display free cell star
    - Tweak win register functions to account for canvas setup

--- pages/canvas.vue
+++ pages/canvas.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="d-flex justify-center">
-    <canvas ref="canvas" :width="SCALE" :height="SCALE" @click="handleClick"></canvas>
-    <input type="color" :value="highlightColor" @change="changeHighlightColor">
+  <div class="d-flex justify-center gap-5">
+    <img ref="starImg" src="~/assets/yellow-star.svg" class="d-none" />
+    <canvas class="cursor-pointer" ref="canvas" :width="SCALE" :height="SCALE" @click="markCell"></canvas>
+    <div id="system">
+      <div id="info">
+        <p :class="wins.rows == 5 ? 'win rows' :wins.rows ? '' : 'disabled'">{{ wins.rows == 5 ? "Rows" : `Rows: ${wins.rows}` }}</p>
+        <p :class="wins.columns == 5 ? 'win columns' :wins.columns ? '' : 'disabled'">{{ wins.columns == 5 ? "Columns" : `Columns: ${wins.columns}` }}</p>
+        <p :class="wins.diagonals == 2 ? 'win diagonals' :wins.diagonals ? '' : 'disabled'">{{ wins.diagonals == 2 ? "Diagonals" : `Diagonals: ${wins.diagonals}` }}</p>
+        <p :class="wins.corners ? 'win corners' : 'disabled'">Corners</p>
+        <p :class="wins.blackout ? 'win blackout' : 'disabled'">Blackout</p>
+      </div>
+      <div id="controls">
+        <button id="reset-btn" @click="resetCard">Reset</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,20 +22,46 @@
   import bingoCard from "./bingo.json";
 
   const canvas = ref(null);
+  const starImg = ref(null);
   const highlightColor = ref("#6df843");
-  const MAXWIDTH = 600;
-  const SCALE = ref(MAXWIDTH);
+  const MAXWIDTH = 560;
+  const SCALE = ref(300);
   const CELLSIZE = ref((SCALE.value/5));
+  const FIRSTRENDER = {value: true};
 
-  // onMounted(() => {
-  //   window.onresize = handleResize;
-  //   handleResize();
-  //   drawCard();
-  // });
+  const rows = ref([null,0,0,0,0,0]),
+    columns = ref([null,0,0,0,0,0]),
+    fDiagonal = ref(0),
+    bDiagonal = ref(0),
+    corners = ref(0),
+    wins = ref({
+      rows: 0,
+      columns: 0,
+      diagonals: 0,
+      corners: 0,
+      blackout: 0,
+    });
 
-  // onUnmounted(() => {
-  //   window.removeEventListener('resize', handleResize);
-  // });
+  const fDiagonalStyle = [[1,5],[2,4],[4,2],[5,1]],
+    bDiagonalStyle = [[1,1],[2,2],[4,4],[5,5]],
+    cornersStyle = [[1,1],[1,5],[5,1],[5,5]],
+    winStyle = {
+      rows: [null,5,5,4,5,5],
+      columns: [null,5,5,4,5,5],
+      fDiagonal: 4,
+      bDiagonal: 4,
+      corners: 4,
+    };
+
+  onMounted(() => { 
+    // resetCard();
+    drawCard();
+    window.onresize = handleResize;
+    handleResize();
+    FIRSTRENDER.value = false;
+  }); 
+
+  onUnmounted(() => { window.removeEventListener('resize', handleResize); });
 
   function drawCard() {
     if (canvas.value) {
@@ -34,7 +72,10 @@
       CELLSIZE.value = SCALE.value / 5;
 
       ctx.clearRect(0, 0, SCALE.value, SCALE.value);
-
+      ctx.beginPath();
+      ctx.lineWidth = 3;
+      ctx.strokeRect(0, 0, SCALE.value, SCALE.value);
+      
       for (let i = 0; i < bingoCard.length; i++) {
         drawCell(ctx, bingoCard[i]);
       }
@@ -42,43 +83,84 @@
   }
 
   function drawCell(ctx, cell) {
-    // const ctx = canvas.value.getContext("2d");
     const x = (cell.row - 1) * CELLSIZE.value,
       y = (cell.column - 1) * CELLSIZE.value;
 
     // Draw the highlight if marked
     if (cell.marked) {
+      // Register the cell if it's marked and it's the first render
+       if (FIRSTRENDER) {registerCell(cell.row, cell.column);}
+
       ctx.beginPath();
       ctx.fillStyle = highlightColor.value;
       ctx.arc(x + CELLSIZE.value / 2, y + CELLSIZE.value / 2, CELLSIZE.value / 2 - 10, 0, Math.PI * 2);
       ctx.fill();
     }
 
+    // Draw the free cell star
+    if (cell.row == 3 && cell.column == 3) {
+      ctx.drawImage(starImg.value, x + 10, y + 10, CELLSIZE.value - 20, CELLSIZE.value - 20);
+    }
+
     // Draw the cell border
     ctx.beginPath();
+    ctx.lineWidth = 1;
     ctx.strokeStyle = "black";
     ctx.strokeRect(x, y, CELLSIZE.value, CELLSIZE.value);
 
     // Draw text
     ctx.fillStyle = 'black';
-    ctx.font = '.5rem Arial';
-    ctx.textAlign = 'center';
+    ctx.font = SCALE.value > 365 ? '16px Arial' : '12px Arial';
+    ctx.textAlign = 'center'; 
     ctx.textBaseline = 'middle';
-    ctx.fillText(cell.value, x + CELLSIZE.value / 2, y + CELLSIZE.value / 2);
+
+    // Calculate vertical centering
+    const textLines = wrapText(ctx, cell.value, CELLSIZE.value - 10);
+    const lineHeight = parseInt(ctx.font, 10) + 2; // Line height based on font size
+    const textBlockHeight = textLines.length * lineHeight;
+    const textY = y + (CELLSIZE.value - textBlockHeight) / 2 + lineHeight / 2;
+
+    // Draw each line of text
+    textLines.forEach((line, index) => {
+      ctx.fillText(line, x + CELLSIZE.value / 2, textY + index * lineHeight);
+    });
   }
 
-  function changeHighlightColor(event) {
-    highlightColor.value = event.target.value;
-    event.target.blur();
+  function wrapText(ctx, text, maxWidth) {
+    const words = text.split(' ');
+    let line = '';
+    const lines = [];
+
+    for (let i = 0; i < words.length; i++) {
+      const testLine = line + words[i] + ' ';
+      const metrics = ctx.measureText(testLine);
+      const testWidth = metrics.width;
+
+      if (testWidth > maxWidth && i > 0) {
+        lines.push(line);
+        line = words[i] + ' ';
+      } else {
+        line = testLine;
+      }
+    }
+
+    lines.push(line);
+    return lines;
   }
 
-  function handleClick(event) {
+  function markCell(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     const [row, column] = getCellCoords(x, y);
     const cell = bingoCard.find(cell => cell.row == row && cell.column == column);
-    
+    // if free cell, don't mark
     if (row == 3 && column == 3) return;
+
+    if (cell.marked) {
+      unregisterCell(cell.row, cell.column);
+    } else {
+      registerCell(cell.row, cell.column);
+    }
     cell.marked = !cell.marked;
 
     drawCard();
@@ -94,7 +176,85 @@
   function handleResize () {
     const newWidth = window.innerWidth * 0.9;
     SCALE.value = newWidth > MAXWIDTH ? MAXWIDTH : newWidth;
-    drawCard();
+    setTimeout(()=>drawCard(),300);
   };
+
+  function registerCell(row, column) {
+    rows.value[row] += 1;
+    columns.value[column] += 1;
+
+    for (let i = 0; i < fDiagonalStyle.length; i++) {
+      if (row == fDiagonalStyle[i][0] && column == fDiagonalStyle[i][1]) fDiagonal.value += 1;
+    }
+    for (let i = 0; i < bDiagonalStyle.length; i++) {
+      if (row == bDiagonalStyle[i][0] && column == bDiagonalStyle[i][1]) bDiagonal.value += 1;
+    }
+    for (let i = 0; i < cornersStyle.length; i++) {
+      if (row == cornersStyle[i][0] && column == cornersStyle[i][1]) corners.value += 1;
+    }
+
+    updateWins();
+  }
+
+  function unregisterCell(row, column) {
+    rows.value[row] -= 1;
+    columns.value[column] -= 1;
+
+    for (let i = 0; i < fDiagonalStyle.length; i++) {
+      if (row == fDiagonalStyle[i][0] && column == fDiagonalStyle[i][1]) fDiagonal.value -= 1;
+    }
+    for (let i = 0; i < bDiagonalStyle.length; i++) {
+      if (row == bDiagonalStyle[i][0] && column == bDiagonalStyle[i][1]) bDiagonal.value -= 1;
+    }
+    for (let i = 0; i < cornersStyle.length; i++) {
+      if (row == cornersStyle[i][0] && column == cornersStyle[i][1]) corners.value -= 1;
+    }
+
+    updateWins();
+  }
+
+  function updateWins() {
+    // Check rows
+    wins.value.rows = rows.value.filter((row, index) => row && row == winStyle.rows[index]).length;
+
+    // Check columns
+    wins.value.columns = columns.value.filter((column, index) => column && column == winStyle.columns[index]).length;
+
+    // Check diagonals
+    wins.value.diagonals = [fDiagonal.value,bDiagonal.value].filter((diagonal,index) => diagonal == [winStyle.fDiagonal,winStyle.bDiagonal][index]).length;
+
+    // Check corners
+    wins.value.corners = [corners.value].filter((corner,index) => corner == [winStyle.corners][index]).length;
+
+    // Blackout
+    if (
+      wins.value.rows == 5 &&
+      wins.value.columns == 5 &&
+      wins.value.diagonals == 2 &&
+      wins.value.corners
+    ) wins.value.blackout = 1;
+  }
+
+  function resetCard() {
+    for (let i = 1; i <= rows.length; i++) {
+      rows.value[i] = 0;
+      columns.value[i] = 0;
+    }
+
+    fDiagonal.value = 0; 
+    bDiagonal.value = 0;
+    corners.value = 0;
+    wins.value.rows = 0;
+    wins.value.columns = 0;
+    wins.value.diagonals = 0;
+    wins.value.corners = 0;
+    wins.value.blackout = 0;
+
+    for (let i = 0; i < bingoCard.length; i++) {
+      bingoCard[i].marked = false;
+    }
+
+    drawCard();
+  }
 
 </script>
