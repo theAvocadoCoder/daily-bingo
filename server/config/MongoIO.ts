d4f264c79e0a20d4cc50ca9e27a6cf351172be85
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-04 23:05:18 +0100

    Add the findUser and findCard methods to the MongoIO class

--- server/config/MongoIO.ts
+++ server/config/MongoIO.ts
@@ -1,8 +1,10 @@
 /***********************************************
  * Class MongoIO implements all mongodb I-O
  */
-import { MongoClient, Db } from "mongodb";
+import { Collection, Db, MongoClient, ObjectId } from "mongodb";
 import MongoInterface from "../interfaces/MongoInterface";
+import User from "../interfaces/User";
+import Card from "../interfaces/Card";
 
 /*************************************************
  * Class Properties
@@ -10,6 +12,8 @@ import MongoInterface from "../interfaces/MongoInterface";
 export default class MongoIO implements MongoInterface {
   private client?: MongoClient;
   private db?: Db;
+  private userCollection?: Collection;
+  private cardCollection?: Collection;
 
   /*************************************************
    * Constructor
@@ -22,11 +26,13 @@ export default class MongoIO implements MongoInterface {
 
   public async connect(): Promise<void> {
     const connectionString = process.env.ATLAS_URI as string;
-    const dbName = "test";
+    const dbName = process.env.DB_NAME;
 
     this.client = new MongoClient(connectionString);
     await this.client.connect();
     this.db = this.client.db(dbName);
+    this.userCollection = this.db.collection("users");
+    this.cardCollection = this.db.collection("cards");
 
     console.log("Database", dbName, "Connected");
   }
@@ -40,6 +46,50 @@ export default class MongoIO implements MongoInterface {
       await this.client.close();
       this.client = undefined;
       this.db = undefined;
+    }
+  }
+
+  /****************************************************
+   * Get user
+   */
+  public async findUser(id: string): Promise<User> {
+    if (!this.userCollection) {
+      throw new Error("findUser - Database not connected");
+    }
+    const userId = new ObjectId(id);
+    const pipeline = [
+      {$match: {_id: userId}}
+    ]
+
+    let results: User | null;
+    results = await this.userCollection.aggregate<User>(pipeline).next();
+
+    if (results === null) {
+      throw new Error("User not found " + id);
+    } else {
+      return results;
+    }
+  }
+
+  /***********************************************
+   * Get a card
+   */
+  public async findCard(id: string): Promise<Card> {
+    if (!this.cardCollection) {
+      throw new Error("findCard - Database not connected");
+    }
+    const cardId = new ObjectId(id);
+    const pipeline = [
+      {$match: {_id: cardId}}
+    ]
+
+    let results: Card | null;
+    results = await this.cardCollection.aggregate<Card>(pipeline).next();
+
+    if (results === null) {
+      throw new Error("Card not found " + id);
+    } else {
+      return results;
     }
   }
 }
--- server/interfaces/MongoInterface.ts
+++ server/interfaces/MongoInterface.ts
@@ -1,4 +1,9 @@
+import Card from "./Card";
+import User from "./User";
+
 export default interface MongoInterface {
   connect(): Promise<void>;
   disconnect(): Promise<void>;
+  findUser(id: string): Promise<User>;
+  findCard(id: string): Promise<Card>;
 }
