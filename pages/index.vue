7428cdb4d009dc9a4550518d34758ce548ff4e8d
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-06 04:36:40 +0100

    Display daily bingo card. Fetch from endpoint if unavailable, or localstorage if available

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
-    <h1 class="">Daily Bingo!</h1>
-    <p>{{ user?.greeting }}, {{ user?.username }}</p>
+    <h1>Daily Bingo!</h1> 
+    <p>
+      {{ user?.greeting }}, {{ user?.displayName }}
+    </p>
 
-    <Bingo :card="card?.cells" />
+    <Bingo :card="dailyBingo?.cells" />
   </div>
 </template>
 
 <script setup lang="ts">
-  const userId = "66d8754397e9d9f8ccb56203";
+  import { ref } from "vue";
+  import type User from "~/server/interfaces/User";
+  import type Card from "~/server/interfaces/Card";
+  // @ts-expect-error
+  import { getData, setData } from "nuxt-storage/local-storage";
 
-  const { data: user } = await useFetch(`/api/users/${userId}`);
-  const userCards = user.value?.cards.created;
-  const { data: card } = (userCards && userCards.length) ?
-    await useFetch(`/api/cards/${userCards[0]}`) : {data: null};
+  interface ReturnUser extends User { greeting: string };
+  interface ReturnCard extends Card { _: "" };
 
+  let user: Ref<ReturnUser>;
+  let userError: unknown;
+  const userInStorage = getData("user");
+
+  let dailyBingo: Ref<ReturnCard>;
+  let cardError: unknown;
+  const cardInStorage = getData("dailyBingo");
+
+  if (userInStorage) {
+    user = ref(userInStorage as ReturnUser)
+  } else {
+    const userId = "66d8754397e9d9f8ccb56203";
+    const { data, error } = await useFetch(`/api/users/${userId}`);
+    user = ref(data.value as unknown as ReturnUser);
+    userError = error.value as Error;
+    setData("user", user.value, 30, "d");
+  }
+
+  if (cardInStorage) {
+    dailyBingo = ref(cardInStorage as ReturnCard);
+  } else {
+    const { data, error } = await useFetch("/api/cards/daily");
+    dailyBingo = ref(data.value as unknown as ReturnCard);
+    cardError = error.value as Error;
+    setData("dailyBingo", dailyBingo.value, 1, "d");
+  }
 </script>
