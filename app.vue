1a98d953ee564979ef56850d34806f0eb1f22a9e
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-10 23:49:56 +0100

    Split layout into mobile and desktop views

--- app.vue
+++ app.vue
@@ -1,10 +1,22 @@
 
 <template>
   <div>
-    <NuxtLayout>
+    <NuxtLayout :name="layout">
       <v-app>
-        <NuxtPage />
-      </v-app>
+      <NuxtPage />
+    </v-app>
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { useDisplay } from "vuetify";
+  import { onMounted, ref } from "vue";
+
+  const layout = ref("mobile");
+  const { lgAndUp } = useDisplay();
+
+  onMounted(() => {
+    if (lgAndUp.value) layout.value = "desktop";
+  })
+</script>
---
+++ layouts/desktop.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar=""
+            :subtitle="`@${user?.user?.username}`"
+            :title="user?.user?.displayName"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-account-group" title="Groups" value="groups"></v-list-item>
+          <v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Cards" value="cards"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main class="overflow-y-auto h-75">
+        <slot />
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+  import { useUserStore } from "../stores/user";
+
+  const user = useUserStore();
+
+</script>
