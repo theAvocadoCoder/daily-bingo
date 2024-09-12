420422dd27229677791e8e67a3d8d80428953cf2
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-12 04:18:15 +0100

    Change modules to use nuxt recommended modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
@@ -12,18 +10,13 @@ export default defineNuxtConfig({
     dirs: ["./stores"],
   },
   modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        //@ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      });
-    },
+    "vuetify-nuxt-module",
+    "@nuxtjs/tailwindcss",
+    // "@pinia/nuxt",
   ],
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
-    },
+  vuetify: {
+    moduleOptions: {
+      includeTransformAssetsUrls: true,
+    }
   },
 });
