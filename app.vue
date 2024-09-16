90ad756ce4bc7b731909ed25c77d76d3fb1c0c4b
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-16 05:49:16 +0100

    Implement mobile view for home page

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 
 <template>
   <div>
-    <NuxtLayout class="max-w-screen" :name="$vuetify.display.lgAndUp ? 'desktop' : 'mobile'">
-      <!-- <v-app> -->
-      <NuxtPage class="h-full p-5 xl:p-10" />
-    <!-- </v-app> -->
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <v-app class="!h-full">
+        <NuxtPage class="p-5 xl:p-10" />
+      </v-app>
     </NuxtLayout>
   </div>
 </template> 
---
+++ layouts/default.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-layout>
+    <v-app-bar
+      class="!bg-stone-950 !text-stone-50 !px-5 !w-full"
+      prominent
+    >
+      <v-app-bar-nav-icon variant="text">
+        <nuxt-link to="/" class="text-2xl">DB</nuxt-link>
+      </v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+
+      <nuxt-link to="/profile">
+        <v-btn icon="mdi-account" variant="text"></v-btn>
+      </nuxt-link>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-if="$vuetify.display.lgAndUp"
+      permanent
+      class="!bg-stone-800 !text-stone-50"
+    >
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://upload.wikimedia.org/wikipedia/commons/b/be/Avocado_Coder.png"
+          :subtitle="`@${user?.username}`"
+          :title="user?.displayName"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item
+          v-for="(item) in navItems"
+          tag="nuxt-link"
+          :to="item.to"
+          :active="item.to == path"
+          :prepend-icon="item.icon"
+          :value="item.value"
+        >
+          <v-list-item-title>{{ item.value.toLocaleUpperCase() }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="h-full">
+      <slot />
+    </v-main>
+
+    <v-bottom-navigation
+      v-if="!$vuetify.display.lgAndUp"
+      mode="shift"
+      class="!bg-stone-950 fixed bottom-0 !w-full"
+    >
+      <v-btn 
+        v-for="(item) in navItems"
+        tag="nuxt-link"
+        :to="item.to"
+        class="!text-stone-50" 
+        :value="item.value"
+      >
+        <v-icon>{{ item.icon }}</v-icon>
+        <span>{{ item.value.toLocaleUpperCase() }}</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+  import { navItems } from "./_data.json";
+
+  const path = ref(useRoute().path)
+
+  // Watch for changes in the route
+  watch(
+    () => useRoute().path,
+    (newPath) => {
+      path.value = newPath;  // Update currentPath whenever the route changes
+    }
+  );
+
+  const {data: user} = await useFetch("/api/users/66d8754397e9d9f8ccb56203");
+  
+
+</script>
