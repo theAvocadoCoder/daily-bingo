d8065beeb1a78bbec11f81c0270a79f0a746d580
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-17 03:42:24 +0100

    Make custom loading component

---
+++ components/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loading-screen">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.loading-screen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  z-index: 1000;
+}
+.spinner {
+  border: 5px solid rgba(0, 0, 0, 0.1);
+  border-left-color: #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+}
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
