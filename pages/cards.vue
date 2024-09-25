5b3de542ab2fa0792a7f0ad398b053eb4feba2a6
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-25 19:37:29 +0100

    Display user's cards in the card on the cards page

--- pages/cards.vue
+++ pages/cards.vue
@@ -1,5 +1,22 @@
 <template>
-  <h1>
-    Your Cards
-  </h1>
+  <div>
+    <h1>
+      Your Cards
+    </h1>
+
+    <ul class="flex flex-col gap-2">
+      <li v-for="(card, index) in sessionUser.cards">
+        <v-card
+          :title="`Bingo Card #${index+1}`"
+          :subtitle="`by ${card.created_by.username}`"
+        ></v-card>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup lang="ts">
+  const {data} = useAuth();
+
+  const sessionUser = computed(() => data.value?.user);
+</script>
