dc9c914241c56c2878cad8896853889113ab51ba
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-09-27 17:19:04 +0100

    Allow users to edit the names of saved cards

--- pages/cards/index.vue
+++ pages/cards/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="h-full bg-lime-50">
     <h1>
       Your Cards
     </h1>
@@ -7,18 +7,89 @@
     <ul class="flex flex-col gap-2">
       <li v-for="(card, index) in sessionUser.cards">
         <v-card
-          tag="nuxt-link"
+          class="!bg-lime-300 hover:!bg-lime-500/80"
+          :tag="editMode == card._id ? 'div' : 'nuxt-link'"
+          @click.prevent="editMode == card._id"
           :to="`/cards/${card._id}`"
-          :title="card.card_name"
-          :subtitle="`by ${card.created_by.username}`"
-        ></v-card>
+        >
+          <v-card-text class="flex justify-between items-center !py-3 !px-2">
+            <div>
+              <v-form v-if="editMode === card._id" @submit.prevent="handleSaveEdit(card._id)" @click.prevent class="flex items-center w-fit px-4">
+                <v-text-field
+                  class="min-w-32 m-0"
+                  v-model="cardModel.card_name"
+                  :placeholder="card.card_name"
+                  label="Card Name"
+                  variant="underlined"
+                ></v-text-field>
+                <v-btn icon="mdi-close" variant="text" :disabled="saving" @click.prevent="handleCancelEdit"></v-btn>
+                <v-btn type="submit" :loading="saving" @click.prevent="handleSaveEdit(card._id)">Save</v-btn>
+              </v-form>
+              <v-card-title v-else class="!text-lg truncate max-w-32 min-[330px]:max-w-40 min-[425px]:max-w-64 sm:max-w-72 md:max-w-96">{{ card.card_name }}</v-card-title>
+              <v-card-subtitle>by {{ `${ card.created_by.user_id ? '@' : '' }${ card.created_by.username }` }}</v-card-subtitle>
+            </div>
+            <div>
+              <v-btn :icon="editMode == card._id ? 'mdi-check' : 'mdi-pencil'" :disabled="editMode && editMode != card._id" variant="text" @click.prevent="handleSaveEdit(card._id)"></v-btn>
+              <v-btn icon="mdi-delete" :disabled="editMode && editMode != card._id" variant="text" @click.prevent="handleDelete(card._id)"></v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-  const {data} = useAuth();
+  const { setData } = useNuxtApp().$locally;
+  const { data, getSession } = useAuth();
 
   const sessionUser = computed(() => data.value?.user);
+  const cardModel = ref({
+    card_name: ""
+  })
+
+  const editMode = ref(false);
+  const saving = ref(false);
+
+  function handleCancelEdit() {
+    cardModel.value.card_name = "";
+    editMode.value = false;
+  }
+
+  async function handleSaveEdit(cardId?) {
+    if (editMode.value == cardId) {
+      saving.value = true;
+
+      if (cardModel.value.card_name == "") {
+        const previousCardName = sessionUser.value.cards.find(card => card._id == cardId).card_name;
+        cardModel.value.card_name = previousCardName;
+      }
+
+      await $fetch(`/api/users/${sessionUser.value._id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: {
+            "cards.$.card_name": cardModel.value.card_name,
+          },
+          filters: {
+            "cards._id": cardId,
+          }
+        }),
+      });
+
+      await getSession(true);
+      setData("bingoUser", sessionUser.value, true);
+      saving.value = false;
+      editMode.value = false;
+    } else {
+      editMode.value = cardId;
+    }
+  }
+
+  async function handleDelete(cardId) {
+    
+  }
 </script>
