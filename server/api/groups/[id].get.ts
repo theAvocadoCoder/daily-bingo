8b44fe48e195923c506a6c57b36edb08bb9e08c0
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-10-07 23:58:39 +0100

    Implement api route for getting a single group

---
+++ server/api/groups/[id].get.ts
@@ -0,0 +1,17 @@
+import { getMessage, mongo } from "~/server/index";
+
+export default defineEventHandler(async (event) => {
+  const groupId = getRouterParam(event, "id") as string;
+
+  try {
+    const theGroup = await mongo.findGroup(groupId);
+    console.info("Get group %s completed", groupId);
+    setResponseStatus(event, 200)
+    return theGroup;
+
+  } catch (error) {
+    let message = getMessage(error);
+    console.info("Get group %s failed because %s", groupId, message);
+    setResponseStatus(event, 500, message);
+  }
+})
