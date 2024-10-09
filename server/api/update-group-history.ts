ed0e81cda97676272b180b9e3d4121e70a60d717
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-10-09 21:58:05 +0100

    Implement route to handle ably integration for updating messages in the DB

---
+++ server/api/update-group-history.ts
@@ -0,0 +1,29 @@
+import { getMessage } from "~/server/index";
+import Ably from "ably";
+
+export default defineEventHandler(async (event) => {
+  const { channel: channelName, messages: encodedMessages } = await readBody(event);
+  const messages = Ably.Realtime.Message.fromEncodedArray(encodedMessages);
+  const groupId = channelName.split("-")[1];
+
+  try {
+    const updatedGroup = await $fetch(`/api/groups/${groupId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        messages,
+        operation: "messages-insert"
+      }),
+    })
+
+    setResponseStatus(event, 200, "Successfully updated group history");
+    return updatedGroup;
+
+  } catch (error) {
+    let message = getMessage(error);
+    console.info("Update Group History failed because %s", message);
+    setResponseStatus(event, 500, message);
+  }
+})
