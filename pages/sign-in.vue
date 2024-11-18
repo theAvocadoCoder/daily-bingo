9cd707bb88dc1c0a74f79275c4504b3d135e0a6d
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-11-18 03:39:53 +0100

    Move sign in and sign up pages to root, refactor to use clerk

---
+++ pages/sign-in.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <SignIn />
+  </div>
+</template>
+
+<script setup>
+definePageMeta({ middleware: 'guest' });
+</script>
---
+++ pages/sign-up.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <SignUp />
+  </div>
+</template>
+
+<script setup>
+definePageMeta({ middleware: 'guest' });
+</script>
