7f3635d1f44e19df8600439b1e28f9497a5c7012
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-11-20 02:13:07 +0100

    Retrieve only relevant user data from clerk

--- server/api/users/current-user.get.ts
+++ server/api/users/current-user.get.ts
@@ -3,17 +3,22 @@ import { clerkClient } from "vue-clerk/server";
 
 export default defineEventHandler(async (event) => {
   const { userId } = event.context.auth;
-  const clerkUser = await clerkClient(event).users.getUser(userId as string);
+  let clerkUser, dbUser;
+  if (userId) clerkUser = await clerkClient(event).users.getUser(userId as string);
 
   if (!clerkUser) {
     // TODO: Properly handle this error
-    console.error("Could not get a response from Clerk about the current user");
+    // console.error("Could not get a response from Clerk about the current user");
+  } else {
+    dbUser = await mongo.findUserByEmail(clerkUser?.emailAddresses[0].emailAddress as string);
   }
 
-  const dbUser = await mongo.findUserByEmail(clerkUser?.emailAddresses[0].emailAddress as string);
-
   return {
-    ...clerkUser,
-    ...dbUser
+    // ...clerkUser,
+    ...{ 
+      first_name: clerkUser?.firstName,
+      last_name: clerkUser?.lastName,
+    }, 
+    ...dbUser,
   }
 })
