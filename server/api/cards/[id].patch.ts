3ff8eb6a0c89921885c04f98ea85da58cbbeb32e
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-11-22 04:22:56 +0100

    Handle deleting card from user's collection in the card delete event handler

--- server/api/cards/[id].patch.ts
+++ server/api/cards/[id].patch.ts
@@ -1,14 +1,25 @@
 import { getMessage, mongo } from "~/server";
+import type User from "~/interfaces/User";
 
 export default defineEventHandler(async (event) => {
   const cardId = getRouterParam(event, "id") as string;
-  const {card, operation} = await readBody(event);
+  const {card, operation, userId} = await readBody(event);
 
   try {
     let theCard;
     if (operation) {
       theCard = await mongo.updateCardReferences(cardId, operation);
       if (card) theCard = await mongo.updateCard(cardId, card);
+      await $fetch<User>(`/api/users/${userId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: theCard._id,
+          operation: "cards-delete",
+        }),
+      });
     } else {
       theCard = await mongo.updateCard(cardId, card);
     }
