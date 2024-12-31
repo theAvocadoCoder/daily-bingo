0e931d858d4558aea75d14cebae362236084b528
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-12-31 17:48:34 +0100

    Fix spacing on landing page welcome message

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -5,14 +5,12 @@
       <p class="italic text-sm">brought to you by <span class="font-medium [font-style:initial]">World of Avocado</span></p>
     </div>
 
-    <div class="flex flex-col justify-center items-center py-5 md:py-7 lg:py-10 text-lg gap-7 md:gap-10 lg:gap-14">
-      <p class="">Rediscover fun friend-group activities</p>
-      <p class="">Create custom bingo cards</p>
-
-      <p class="">Bingo cards are a grid of cells with data in them, could be numbers, emojis, names, anything you want!</p>
-      <p class="">You play Daily Bingo by generating a bingo card with your choice of data and marking a cell whenever you encounter its contents.</p>
-
-      <p class="">For example, a bingo card cell that has the content, "Black swan" can be marked when you see a black swan, perhaps when you're walking by your neighborhood lake.</p>
+    <div class="flex flex-col justify-center items-center text-center max-w-xl mx-auto py-5 md:py-7 lg:py-10 text-lg gap-5 md:gap-7 lg:gap-10">
+      <p class="">
+        Bingo cards are a grid of cells with data in them, could be numbers, emojis, names, anything you want!
+        <br><br>You play Daily Bingo by generating a bingo card with your choice of data and marking a cell whenever you encounter its contents.
+        <br><br>For example, a bingo card cell that has the content, "Black swan" can be marked when you see a black swan, perhaps when you're walking by your neighborhood lake.
+      </p>
 
       <div class="">
         <p class="">Want to see what that's like?</p>
